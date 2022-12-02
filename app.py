e779f7385addec90be4bb663ae0e64e4b5ced938
Pratt <pratt.paredes@gmail.com>
2022-12-02 10:53:33 -0500

    some types of variables

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-print("hello world")
+# print("hello world")
 
-print("    /|")
-print("   / |")
-print("  /  |")
-print(" /___|")
+# print("    /|")
+# print("   / |")
+# print("  /  |")
+# print(" /___|")
+
+character_name = "Pratt"
+character_age = "23"
+age = 23
+is_male = False
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+print("he really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age)
