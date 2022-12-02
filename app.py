892ccb8590dad9b5c05f77239cb924d693c5277b
Pratt <pratt.paredes@gmail.com>
2022-12-02 11:07:48 -0500

    some operations with strings and numbers

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ character_age = "23"
 age = 23
 is_male = False
 
-print("There once was a man named " + character_name + ", ")
-print("he was " + character_age + " years old.")
-print("he really liked the name " + character_name + ", ")
-print("but didn't like being " + character_age)
+# print("There once was a man named " + character_name + ", ")
+# print("he was " + character_age + " years old.")
+# print("he really liked the name " + character_name + ", ")
+# print("but didn't like being " + character_age)
+
+phrase = "Giraffe Academy"
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("c"))
+print(phrase.replace("Giraffe", "Elephant"))
+print(phrase)
+print(-2.75)
+print(3 + 5)
+print(3 * 4 + 5)
+print(10 % 3)
+my_num = 5
+print("I have " + str(my_num) + " kids")
