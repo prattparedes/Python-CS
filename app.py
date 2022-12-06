7c93883707198c116d4bc5784d6e93da1626fc1c
Pratt <pratt.paredes@gmail.com>
2022-12-06 01:03:41 -0500

    some more operators

--- app.py
+++ app.py
@@ -5,25 +5,31 @@
 # print("  /  |")
 # print(" /___|")
 
-character_name = "Pratt"
-character_age = "23"
-age = 23
-is_male = False
+# character_name = "Pratt"
+# character_age = "23"
+# age = 23
+# is_male = False
 
 # print("There once was a man named " + character_name + ", ")
 # print("he was " + character_age + " years old.")
 # print("he really liked the name " + character_name + ", ")
 # print("but didn't like being " + character_age)
 
-phrase = "Giraffe Academy"
-print(len(phrase))
-print(phrase[0])
-print(phrase.index("c"))
-print(phrase.replace("Giraffe", "Elephant"))
-print(phrase)
-print(-2.75)
-print(3 + 5)
-print(3 * 4 + 5)
-print(10 % 3)
-my_num = 5
+# phrase = "Giraffe Academy"
+# print(len(phrase))
+# print(phrase[0])
+# print(phrase.index("c"))
+# print(phrase.replace("Giraffe", "Elephant"))
+# print(phrase)
+# print(-2.75)
+# print(3 + 5)
+# print(3 * 4 + 5)
+# print(10 % 3)
+
+my_num = -5
 print("I have " + str(my_num) + " kids")
+print(abs(my_num))
+print(pow(3,2))
+print(max(4,100))
+print(min(4,100))
+print(round(3.49))
