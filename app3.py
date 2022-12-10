1070e4f52c98070276bb4cb3e688fe81bfef3ac5
Pratt <pratt.paredes@gmail.com>
2022-12-09 23:29:57 -0500

    dictionaries

--- app3.py
+++ app3.py
@@ -1,17 +1,39 @@
 #calculator
-num1 = float(input("Enter first number: "))
-op = input("Enter operator: ")
-num2 = float(input("Enter second number: "))
+# num1 = float(input("Enter first number: "))
+# op = input("Enter operator: ")
+# num2 = float(input("Enter second number: "))
 
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
-elif op == "/":
-    print(num1 / num2)
-elif op == "*":
-    print(num1 * num2)
-else:
-    print("Invalid operator")
+# if op == "+":
+#     print(num1 + num2)
+# elif op == "-":
+#     print(num1 - num2)
+# elif op == "/":
+#     print(num1 / num2)
+# elif op == "*":
+#     print(num1 * num2)
+# else:
+#     print("Invalid operator")
 
 # Dictionaries
+monthConversions = {
+     0: "Monday",
+     1: "Tuesday",
+     2: "Wednesday",
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions["Nov"])
+print(monthConversions.get("Dxc", "Not a valid Key"))
+print(monthConversions.get(0, "Not a valid Key"))
+print(monthConversions.get(3, "Not a valid Key"))
