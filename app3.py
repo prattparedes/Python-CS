38f76bf5bd73bc429183ba359bb6b7fd26f10805
Pratt <pratt.paredes@gmail.com>
2022-12-09 23:47:32 -0500

    for loops

--- app3.py
+++ app3.py
@@ -53,14 +53,38 @@ guess_count = 0
 guess_limit = 3
 out_of_guesses = False
 
-while guess != secret_word and not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter guess: ")
-        guess_count += 1
-    else:
-        out_of_guesses = True
+# while guess != secret_word and not(out_of_guesses):
+#     if guess_count < guess_limit:
+#         guess = input("Enter guess: ")
+#         guess_count += 1
+#     else:
+#         out_of_guesses = True
 
-if out_of_guesses:
-    print("Out of Guesses, YOU LOSE!")
-else:
-    print("You win!")
+# if out_of_guesses:
+#     print("Out of Guesses, YOU LOSE!")
+# else:
+#     print("You win!")
+
+
+#For Loop
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["Jim", "Karen", "Kevin"]
+for name in friends:
+    print(name)
+
+#range(3,10)
+for index in range(5):
+    print(index)
+for index in range(3, 10):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print('first iteration')
+    else:
+        print('Not first')
