937820c8903d3feecaef778d3f0a298169fbc30b
Pratt <pratt.paredes@gmail.com>
2022-12-10 19:37:23 -0500

    little translating app

--- app3.py
+++ app3.py
@@ -98,15 +98,31 @@ out_of_guesses = False
 
 # print(raise_to_power(3, 4))
 
-#2D Lists and nested loops
-number_grid = [
-    [1 ,2 ,3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [0]
-]
+# 2D Lists and nested loops
+number_grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0]]
 
-print(number_grid[2][2])
-for row in number_grid:
-    for col in row:
-        print(col)
+# print(number_grid[2][2])
+# for row in number_grid:
+#     for col in row:
+#         print(col)
+
+# Build a translator
+# Giraffe Language
+# vowels -> g
+
+
+def translate(phrase):
+    translation = ""
+
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
