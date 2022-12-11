1fde7ddd0bd57ebb1be06b9b4d0264907b0cc6b2
Pratt <pratt.paredes@gmail.com>
2022-12-10 19:10:51 -0500

    2DLists nested loop

--- app3.py
+++ app3.py
@@ -90,10 +90,23 @@ out_of_guesses = False
 #         print('Not first')
 
 # Exponent functions
-def raise_to_power(base_num, pow_num):
-    result = 1
-    for index in range(pow_num):
-        result = result * base_num
-    return result
+# def raise_to_power(base_num, pow_num):
+#     result = 1
+#     for index in range(pow_num):
+#         result = result * base_num
+#     return result
 
-print(raise_to_power(3, 4))
+# print(raise_to_power(3, 4))
+
+#2D Lists and nested loops
+number_grid = [
+    [1 ,2 ,3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+print(number_grid[2][2])
+for row in number_grid:
+    for col in row:
+        print(col)
