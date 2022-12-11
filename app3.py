dd90c1fb636de092a8d24b833897c77218b95a73
Pratt <pratt.paredes@gmail.com>
2022-12-10 21:14:40 -0500

    Try/except

--- app3.py
+++ app3.py
@@ -125,4 +125,17 @@ def translate(phrase):
     return translation
 
 
-print(translate(input("Enter a phrase: ")))
+# print(translate(input("Enter a phrase: ")))
+
+# Try / Except
+
+
+try:
+    value = 10/0
+    number = int(input("Enter a number: "))
+    print(number)
+except ZeroDivisionError as err:
+    print(err)
+    print("Divided by zero")
+except ValueError:
+    print("Invalid input")
