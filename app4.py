f7d313c11d021f3b555335710d22acac8cb7cdf0
Pratt <pratt.paredes@gmail.com>
2022-12-11 21:48:34 -0500

    classes and objects

--- app4.py
+++ app4.py
@@ -4,3 +4,15 @@ import docx
 
 print(useful_tools.roll_dice(10))
 # docx.
+
+# Classes and Objects
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Mike", "Sciences", 4.1, True)
+student3 = Student("Lara", "Art", 2.3, True)
+
+print(student1)
+print(student1.gpa)
+print(student2.name)
+print(student3.major)
